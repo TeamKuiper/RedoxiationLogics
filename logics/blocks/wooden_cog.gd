f701b7b22d3d5262224d473384c5532a10ab4f17
Kuiprux <kuiprux@gmail.com>
2022-05-03 10:03:41 +0900

    Added legacy cog logic

--- logics/blocks/wooden_cog.gd
+++ logics/blocks/wooden_cog.gd
@@ -3,11 +3,39 @@ extends "res://logics/blocks/block.gd"
 func on_block_action(_world, _vec2, _action):
 	pass
 
-func on_block_placed(_world, _vec2):
-	pass
+func on_block_placed(world, vec2):
+	var tile = world.get_tile_entity(vec2)
+	var check = tile.fill(vec2, 0, 1)
+	tile.set_fill(world, vec2, check, 0)
 
-func on_block_broken(_world, _vec2):
-	pass
+func on_block_broken(world, vec2):
+	var check = 0
+	
+	if check_state(world, Vector2(vec2.x+1, vec2.y), 1):
+		world.get_tile_entity(Vector2(vec2.x+1, vec2.y)).fill(Vector2(vec2.x+1, vec2.y), 0, 1)
+	if check_state(world, Vector2(vec2.x-1, vec2.y), 1):
+		world.get_tile_entity(Vector2(vec2.x-1, vec2.y)).fill(Vector2(vec2.x-1, vec2.y), 0, 1)
+	if check_state(world, Vector2(vec2.x, vec2.y+1), 1):
+		world.get_tile_entity(Vector2(vec2.x, vec2.y+1)).fill(Vector2(vec2.x, vec2.y+1), 0, 1)
+	if check_state(world, Vector2(vec2.x, vec2.y-1), 1):
+		world.get_tile_entity(Vector2(vec2.x, vec2.y-1)).fill(Vector2(vec2.x, vec2.y-1), 0, 1)
+
+	if check_state(world, Vector2(vec2.x+1, vec2.y), 0):
+		check = world.get_tile_entity(Vector2(vec2.x+1, vec2.y)).chunk_number
+		world.get_tile_entity(Vector2(vec2.x+1, vec2.y)).set_fill(world, Vector2(vec2.x+1, vec2.y), check, 0)
+	if check_state(world, Vector2(vec2.x-1, vec2.y), 0):
+		check = world.get_tile_entity(Vector2(vec2.x-1, vec2.y)).chunk_number
+		world.get_tile_entity(Vector2(vec2.x-1, vec2.y)).set_fill(world, Vector2(vec2.x-1, vec2.y), check, 0)
+	if check_state(world, Vector2(vec2.x, vec2.y+1), 0):
+		check = world.get_tile_entity(Vector2(vec2.x, vec2.y+1)).chunk_number
+		world.get_tile_entity(Vector2(vec2.x, vec2.y+1)).set_fill(world, Vector2(vec2.x, vec2.y+1), check, 0)
+	if check_state(world, Vector2(vec2.x, vec2.y-1), 0):
+		check = world.get_tile_entity(Vector2(vec2.x, vec2.y-1)).chunk_number
+		world.get_tile_entity(Vector2(vec2.x, vec2.y-1)).set_fill(world, Vector2(vec2.x, vec2.y-1), check, 0)
 
 func create_tile_entity():
 	return create_tile_entity_by_name("wooden_cog")
+
+
+func check_state(world, vec2, st):
+	return world.get_block(vec2) == world.block_name_to_id['wooden_cog'] and world.get_tile_entity(vec2).state != st
--- logics/tileentities/wooden_cog.gd
+++ logics/tileentities/wooden_cog.gd
@@ -1,4 +1,53 @@
 extends "res://logics/tileentities/tileentity.gd"
 
+var chunk_number = 0
+var ang_vel = 0
+var state
+
 func on_update():
-	pass
+	if chunk_number != 0:
+		ang_vel = 1.0 / chunk_number
+	else:
+		ang_vel = 0
+	print(xPos, " ", yPos, " ", ang_vel)
+	
+	rotation_degrees += ang_vel
+	if rotation_degrees >= 360:
+		rotation = rotation - 360
+	
+func check_state(vec2, st):
+	return world.get_block(vec2) == world.block_name_to_id['wooden_cog'] and world.get_tile_entity(vec2).state != st
+
+func fill(vec2, check_num, st):
+	check_num += 1
+	var tile = world.get_tile_entity(vec2)
+	tile.state = st
+	
+	if check_state(Vector2(vec2.x+1, vec2.y), st):
+		check_num = fill(Vector2(vec2.x+1, vec2.y), check_num, st)
+	if check_state(Vector2(vec2.x-1, vec2.y), st):
+		check_num = fill(Vector2(vec2.x-1, vec2.y), check_num, st)
+	if check_state(Vector2(vec2.x, vec2.y+1), st):
+		check_num = fill(Vector2(vec2.x, vec2.y+1), check_num, st)
+	if check_state(Vector2(vec2.x, vec2.y-1), st):
+		check_num = fill(Vector2(vec2.x, vec2.y-1), check_num, st)
+	
+	tile.chunk_number = check_num
+	return check_num
+
+func set_fill(world, vec2, check_num, st):
+	var tile = world.get_tile_entity(vec2)
+	tile.state = st
+	tile.chunk_number = check_num
+	
+	if check_state(Vector2(vec2.x+1, vec2.y), st):
+		set_fill(world, Vector2(vec2.x+1, vec2.y), check_num, st)
+	if check_state(Vector2(vec2.x-1, vec2.y), st):
+		set_fill(world, Vector2(vec2.x-1, vec2.y), check_num, st)
+	if check_state(Vector2(vec2.x, vec2.y+1), st):
+		set_fill(world, Vector2(vec2.x, vec2.y+1), check_num, st)
+	if check_state(Vector2(vec2.x, vec2.y-1), st):
+		set_fill(world, Vector2(vec2.x, vec2.y-1), check_num, st)
+	
+	tile.chunk_number = check_num
+	return check_num
